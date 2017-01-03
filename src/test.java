f4d7b79fa85c841157e52d34063969667f25eb26
cvvgsk <cvvgsk@gmail.com>
2017-01-03 20:40:59 +0530

    Update test.java

--- src/test.java
+++ src/test.java
@@ -1,9 +1,13 @@
 
-public class test {
+public class Test{       
+ static{       
+ print(10);       
+ }    
+ static void print (int x) {   
+ System.out.println (x); 
+ System.exit(0);   
+  } 
+} 
+//d. It will run with no output
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+	
